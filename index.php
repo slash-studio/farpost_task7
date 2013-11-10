f80ec71159ed359c1f8293b2e03ffceea4e00e29
Mark Tertishniy <mtertishniy@gmail.com>
2013-11-10 12:42:07 +1100

    Marge branches, yo

--- index.php
+++ index.php
@@ -3,5 +3,14 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/settings.php';
 
 $smarty = new TSmarty();
 $smarty->force_compile = true;
+
+$html = file_get_html('http://primpogoda.ru/');
+
+$usd = $html->find('#courceUSD');
+$usd = $html->find('#courceUSD');
+// courceEUR
+// courceCNY
+// courceJPY
+
 $smarty->display('html.tpl');
 ?>
