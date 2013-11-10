84f2c0f63a184157fce7705625c597677446db32
Mark Tertishniy <mtertishniy@gmail.com>
2013-11-10 15:04:58 +1100

    Add weather

--- index.php
+++ index.php
@@ -4,15 +4,15 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/includes/simple_html_dom.php';
 
 $smarty = new TSmarty();
 $smarty->force_compile = true;
-$source = 'page.html';
+$prim_source = 'prim_page.html';
 // $source = 'http://primpogoda.ru/';
-$html = file_get_html('page.html');
+$prim_html = file_get_html($prim_source);
 
 //валюты
 function ParseCurrencies($name)
 {
-   global $html;
-   $result = $html->find('#cource' . $name . ' div.value', 0)->plaintext;
+   global $prim_html;
+   $result = $prim_html->find('#cource' . $name . ' div.value', 0)->plaintext;
    return $result;
 }
 $cur_names = Array('USD', 'EUR', 'CNY', 'JPY');
@@ -22,13 +22,16 @@ foreach ($cur_names as $cur) {
 
 //радиационный фон
 $radiation = Array(
-   'amount'  => $html->find('div.radiation span', 0)->plaintext,
-   'comment' => $html->find('div.radiation div.comment', 0)->plaintext
+   'amount'  => $prim_html->find('div.radiation span', 0)->plaintext,
+   'comment' => $prim_html->find('div.radiation div.comment', 0)->plaintext
 );
 
 //новости
 $news = Array();
-foreach ($html->find('div.news div.news-item') as $div) {
+$max_news_amount = 6;
+$cur_news_amount = 0;
+foreach ($prim_html->find('div.news div.news-item') as $div) {
+   if ($cur_news_amount == $max_news_amount) break;
    if (count(explode(' ', $div->class)) > 1) continue;
    $new = Array();
    $new['href']   = $div->find('a.news-pic', 0)->href;
@@ -36,7 +39,29 @@ foreach ($html->find('div.news div.news-item') as $div) {
    $new['date']   = $div->find('div.time', 0)->plaintext;
    $new['header'] = $div->find('.news-header', 0)->plaintext;
    $news[] = $new;
+   $cur_news_amount++;
 }
 
+//погода
+$weather = Array();
+foreach ($prim_html->find('.short-forecast .widget-body div') as $div) {
+   $day                = Array();
+   $day_style          = $div->find('.widget-weather', 0)->style;
+   $img_s              = substr($day_style, strpos($day_style, '/img/'));
+   $day['icon']        = substr($img_s, 0, strlen($img_s) - 1);
+   $day['temperature'] = $div->find('.widget-temp', 0)->plaintext;
+   $date               = $div->find('.widget-date', 0);
+   $day['date']        = $date->title;
+   $day['week_date']   = $date->plaintext;
+   $weather[] = $day;
+}
+
+//события города
+$vl_poster_source = 'vl_poster_page.html';
+$vl_poster_html = file_get_html($vl_poster_source);
+$posters = Array();
+foreach ($vl_poster_html->find('div.common-bar') as $div) {
+
+}
 $smarty->display('html.tpl');
 ?>
